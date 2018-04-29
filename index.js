9e967e7042496c20cc2cf1f33c086e9fa164784b
Nick Redmond <nicholas.john.redmond@gmail.com>
2018-04-29 14:24:57 -0600

    implemented logic/structure for returning 'too young' info

--- index.js
+++ index.js
@@ -62,16 +62,65 @@ var getTopStories = function() {
     return TOP_STORIES;
 };
 
+var TOO_YOUNG_INFO = [
+    {
+        "victim_name": "Noni",
+        "victim_age": 32,
+        "imageFilename": "noni.jpg",
+        "whyquit_link": "http://whyquit.com/whyquit/A_Noni.html"
+    },
+    {
+        "victim_name": "Kim",
+        "victim_age": 44,
+        "imageFilename": "kim.jpg",
+        "whyquit_link": "http://whyquit.com/whyquit/A_Kim.html"
+    },
+    {
+        "victim_name": "Quentin",
+        "victim_age": 23,
+        "imageFilename": "quentin.jpg",
+        "whyquit_link": "http://whyquit.com/whyquit/A_Quentin.html"
+    },
+    {
+        "victim_name": "Deborah",
+        "victim_age": 38,
+        "imageFilename": "deborah.jpg",
+        "whyquit_link": "http://whyquit.com/whyquit/A_Deborah.html"
+    },
+    {
+        "victim_name": "Bryan",
+        "victim_age": 33,
+        "imageFilename": "bryan.jpg",
+        "whyquit_link": "http://whyquit.com/whyquit/BryanLeeCurtis.html"
+    },
+    {
+        "victim_name": "Carrie",
+        "victim_age": 33,
+        "imageFilename": "carrie.jpg",
+        "whyquit_link": "http://whyquit.com/whyquit/notables.html"
+    }
+];
+var getTooYoungInfo = function() {
+    return TOO_YOUNG_INFO;
+};
+
 var allowAnyOrigin = function(response) {
     response.set("Access-Control-Allow-Origin", "*");
 };
 
-app.get("/whyquit/images/:imageName", (request, response) => {
-    var imageName = request.params.imageName;
-    var img = fs.readFileSync("./images/" + imageName);
+var returnImageData = function(imageName, imageDirectory, response) {
+    var img = fs.readFileSync(imageDirectory + "/" + imageName);
     allowAnyOrigin(response);
     response.writeHead(200, { "Content-Type": "img/jpg" });
     response.end(img, "binary");
+};
+
+app.get("/whyquit/images/:imageName", (request, response) => {
+    returnImageData(request.params.imageName, "./images", response);
+});
+
+app.get("/whyquit/images/too-young/:imageName", (request, response) => {
+    returnImageData(request.params.imageName, "./images/too-young", response);
 });
 
 app.get("/whyquit/top-stories", (request, response) => {
@@ -85,6 +134,12 @@ app.get("/whyquit/secondary-stories", (request, response) => {
     allowAnyOrigin(response);
     response.send(secondaryStories);
 })
+
+app.get("/whyquit/too-young", (request, response) => {
+    var tooYoungInfo = getTooYoungInfo();
+    allowAnyOrigin(response);
+    response.send(tooYoungInfo);
+});
 
 app.listen(port, (err) => {
     if (err) {
