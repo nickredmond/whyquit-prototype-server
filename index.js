030cb0d544ead08fecf35003f84ff43fce157806
Nick Redmond <nicholas.john.redmond@gmail.com>
2018-04-29 08:59:50 -0600

    added endpoint for secondary stories

--- index.js
+++ index.js
@@ -27,13 +27,23 @@ const fs = require("fs");
 
 var SECONDARY_STORIES = [
     {
-        "title": "",
-        "description": "",
-        "whyquit_link": ""
+        "title": "Quitting for New Year's?",
+        "description": "Why 12 out of 13 New Year's quit smoking resolutions fail",
+        "whyquit_link": "http://whyquit.com/pr/122711.html"
+    },
+    {
+        "title": "Dangers of Nicotine Replacement",
+        "description": "10 studies that scream, \"Leave replacement nicotine alone!\"",
+        "whyquit_link": "http://whyquit.com/pr/010115.html"
+    },
+    {
+        "title": "Thinking about E-Cigarettes?",
+        "description": "Ten reasons to try cold-turkey before e-cigarettes",
+        "whyquit_link": "http://whyquit.com/pr/101413.html"
     }
 ];
 var getSecondaryStories = function() {
-    
+    return SECONDARY_STORIES;
 };
 
 var TOP_STORIES = [
@@ -69,6 +79,12 @@ app.get("/whyquit/top-stories", (request, response) => {
     allowAnyOrigin(response);
     response.send(topStories);
 });
+
+app.get("/whyquit/secondary-stories", (request, response) => {
+    var secondaryStories = getSecondaryStories();
+    allowAnyOrigin(response);
+    response.send(secondaryStories);
+})
 
 app.listen(port, (err) => {
     if (err) {
